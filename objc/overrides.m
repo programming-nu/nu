9640fd52b1846473d24abe3c402cfc60b4dcd205
Tim Burks <tim@neontology.com>
2008-01-05 16:22:27 -0800

    Partial workaround for missing x86_64 mach_override.
    I have a partial port of mach_override to x86_64,
    but it still has a few problems (such as where to
    put the branch island), so in the meantime, here's
    a quick-and-dirty replacement that we can use to
    continue to temporarily disable NSLog when allocated
    but uninitialized views are retained.  It just
    writes a ret opcode to the beginning of NSLog and
    saves the old value.
    
    Runtime code modification is fun, but supporting
    resources are seriously lacking.  I'm still missing
    a good opcode/mnemonic converter and a guide to
    memory layout in darwin programs.

--- objc/overrides.m
+++ objc/overrides.m
@@ -6,6 +6,51 @@
 #include "mach_override.h"
 #include "Foundation/Foundation.h"
 
+#ifdef __x86_64__
+
+// Since mach_override is not yet available for x86_64,
+// use a quick-and-dirty substitute.
+
+#include <mach-o/dyld.h>
+
+static void *ptrToNSLog = 0;
+static unsigned char firstByteOfNSLog = 0;
+
+void nu_disableNSLog()
+{
+    int err = 0;
+    if (ptrToNSLog == 0) {
+        _dyld_lookup_and_bind("_NSLog", (void*) &ptrToNSLog, NULL);
+        if (ptrToNSLog) {
+            err = vm_protect( mach_task_self(),
+                (vm_address_t) ptrToNSLog,
+                sizeof(long), false, (VM_PROT_ALL | VM_PROT_COPY) );
+            if (err)
+                err = vm_protect( mach_task_self(),
+                    (vm_address_t) ptrToNSLog, sizeof(long), false,
+                    (VM_PROT_DEFAULT | VM_PROT_COPY) );
+        }
+    }
+    if (!ptrToNSLog || err) {
+        NSLog(@"failed to control NSLog");
+    }
+    else if (!firstByteOfNSLog) {
+        firstByteOfNSLog = *((unsigned char *)ptrToNSLog);
+        // this is not guaranteed to be atomic, so it's thread-unsafe
+        *((unsigned char *)ptrToNSLog) = 0xc3;    // ret
+    }
+}
+
+void nu_enableNSLog()
+{
+    if (ptrToNSLog) {
+        *((unsigned char *)ptrToNSLog) = firstByteOfNSLog;
+        firstByteOfNSLog = 0;
+    }
+}
+
+#else
+
 typedef void (*NSLogPtr)(NSString *format, ...);
 static NSLogPtr g_originalNSLog;
 static BOOL g_enableNSLog;
@@ -36,3 +81,4 @@ void nu_enableNSLog()
 {
     g_enableNSLog = true;
 }
+#endif
