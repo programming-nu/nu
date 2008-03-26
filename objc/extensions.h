8c4fbf7fb77b7ae66ff9078cf96cc80601fdb08d
Tim Burks <tim@neontology.com>
2008-03-25 18:25:10 -0700

    Header declarations and documentation for upTo:do: and downTo:do:.

--- objc/extensions.h
+++ objc/extensions.h
@@ -125,6 +125,18 @@ limitations under the License.
     Iteration counts begin at zero and end at n-1.
  */
 - (id) times:(id) block;
+/*!
+    Iterate from the current value up to a specified limit.
+    On each iteration, evaluate the given block after passing in the index.
+    Indices begin at the receiver's value and end at the specified number.
+ */
+- (id) upTo:(id) number do:(id) block;
+/*!
+    Iterate from the current value down to a specified limit.
+    On each iteration, evaluate the given block after passing in the index.
+    Indices begin at the receiver's value and end at the specified number.
+ */
+- (id) downTo:(id) number do:(id) block;
 @end
 
 /*!
