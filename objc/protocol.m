fd6b505f4081374ebc439f2da1817e2a54318652
Tim Burks <tim@neontology.com>
2008-04-09 09:44:19 -0700

    iPhone related changes.
    Here are the file modifications that go with my previous commit.

--- objc/protocol.m
+++ objc/protocol.m
@@ -15,6 +15,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 #ifdef LINUX
 #define true 1
 #define false 0
@@ -39,6 +40,7 @@ struct objc_method_description_list
 };
 #endif
 
+#ifndef IPHONE
 #ifndef __x86_64__
 
 @interface Protocol : NSObject
@@ -341,4 +343,5 @@ void nu_initProtocols()
 }
 
 @end
+#endif
 #endif
