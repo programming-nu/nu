2b9142078ba11ee1732889a814f1a15dc1cceea5
chapados <chapados@sciencegeeks.org>
2008-06-12 23:22:34 -0700

    Added exponentiation operator '**' and extra NuMath functions for rounding.

--- objc/extensions.m
+++ objc/extensions.m
@@ -594,10 +594,19 @@ extern id Nu__null;
 + (double) cos: (double) x {return cos(x);}
 + (double) sin: (double) x {return sin(x);}
 + (double) sqrt: (double) x {return sqrt(x);}
++ (double) cbrt: (double) x {return cbrt(x);}
 + (double) square: (double) x {return x*x;}
 + (double) exp: (double) x {return exp(x);}
++ (double) exp2: (double) x {return exp2(x);}
 + (double) log: (double) x {return log(x);}
++ (double) log2: (double) x {return log2(x);}
++ (double) log10: (double) x {return log10(x);}
 
++ (double) floor: (double) x {return floor(x);}
++ (double) ceil: (double) x {return ceil(x);}
++ (double) round: (double) x {return round(x);}
+
++ (double) raiseNumber: (double) x toPower: (double) y {return pow(x, y);} 
 + (int) integerDivide:(int) x by:(int) y {return x / y;}
 + (int) integerMod:(int) x by:(int) y {return x % y;}
 
