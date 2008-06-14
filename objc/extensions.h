e9a96cd71fc80b60eccaac6b0b67f65d86c43711
Brian Chapados <chapados@sciencegeeks.org>
2008-06-13 21:26:48 -0700

    Added method declarations and descriptions for additional NuMath functions.

--- objc/extensions.h
+++ objc/extensions.h
@@ -203,10 +203,24 @@ limitations under the License.
 + (double) sqrt: (double) x;
 /*! Get the square of a number. */
 + (double) square: (double) x;
+/*! Get the cubed root of a number. */
++ (double) cbrt: (double) x;
 /*! Get the cosine of an angle. */
 + (double) cos: (double) x;
 /*! Get the sine of an angle. */
 + (double) sin: (double) x;
+/*! Get the largest integral value that is not greater than x.*/
++ (double) floor: (double) x;
+/*! Get the smallest integral value that is greater than or equal to x.*/
++ (double) ceil: (double) x;
+/*! Get the integral value nearest to x by always rounding half-way cases away from zero. */
++ (double) round: (double) x;
+/*! Raise x to the power of y */
++ (double) raiseNumber: (double) x toPower: (double) y;
+/*! Get the qouteint of x divided by y as an integer */
++ (int) integerDivide:(int) x by:(int) y;
+/*! Get the remainder of x divided by y as an integer */
++ (int) integerMod:(int) x by:(int) y;
 /*! Get a random integer. */
 + (long) random;
 /*! Seed the random number generator. */
