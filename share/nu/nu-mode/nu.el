fab700c138749e9bc773ef345b5f354d49750312
Aleksandr Skobelev <al.skobelev@gmail.com>
2009-09-19 11:48:00 +0400

    Removed INCF macro as it broke SLIME

--- share/nu/nu-mode/nu.el
+++ share/nu/nu-mode/nu.el
@@ -745,10 +745,6 @@ See `run-hooks'."
                       (backward-char)
                       (nu-backward-sexp1)) )))))))))
 
-;; incf is not defined in GNU Emacs 22.1.1 (mac-apple-darwin, Carbon Version 1.6.0)
-;; and possibly elsewhere.
-(defmacro incf (x)
-  `(setq ,x (+ ,x 1)))
 
 ;; FORWARD-SEXP ----------------------------------------------------------------
 (defun nu-forward-sexp (&optional arg)
