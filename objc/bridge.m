5cace62155828d6bb080acc30db780ed99a315b3
Philip White <philipwhite@mac.com>
2009-12-03 20:34:06 -0600

    Added basic support for wrapping Nu blocks with C blocks.

--- objc/bridge.m
+++ objc/bridge.m
@@ -1018,9 +1018,22 @@ id nu_calling_objc_method_handler(id target, Method_t m, NSMutableArray *args)
     id result = [NSNull null];
 
     // dynamically construct the method call
+
+//the method_***** functions seems to count c blocks twice, i.e. they separate
+//the @ and ?. Using an NSMethodSignature seems to be an easy way around it.
+//Apple documentation warns (vaguely) about implementation differences
+//with the NSMethodSignature methods
+#define USE_SIG 1
+
+#if USE_SIG
+	NSMethodSignature *sig = [target methodSignatureForSelector:s];
+	int argument_count = [sig numberOfArguments];
+#else
     int argument_count = method_getNumberOfArguments(m);
-    if ( [args count] != argument_count-2) {
-        raise_argc_exception(s, argument_count-2, [args count]);
+#endif
+	if ( [args count] != argument_count-2) {
+		
+		raise_argc_exception(s, argument_count-2, [args count]);
     }
     else {
         bool success = false;
@@ -1034,8 +1047,13 @@ id nu_calling_objc_method_handler(id target, Method_t m, NSMutableArray *args)
         int *argument_needs_retained = (int *) malloc (argument_count * sizeof(int));
         int i;
         for (i = 0; i < argument_count; i++) {
+			#if USE_SIG
+			strncpy(&arg_type_buffer[0], [sig getArgumentTypeAtIndex:i], BUFSIZE);
+			#else
             method_getArgumentType(m, i, &arg_type_buffer[0], BUFSIZE);
-            argument_types[i] = ffi_type_for_objc_type(&arg_type_buffer[0]);
+            #endif
+			
+			argument_types[i] = ffi_type_for_objc_type(&arg_type_buffer[0]);
             argument_values[i] = value_buffer_for_objc_type(&arg_type_buffer[0]);
             if (i == 0)
                 *((id *) argument_values[i]) = target;
@@ -1449,6 +1467,8 @@ id add_method_to_class(Class c, NSString *methodName, NSString *signature, NuBlo
 
 @end
 
+
+
 static NuSymbol *oneway_symbol, *in_symbol, *out_symbol, *inout_symbol, *bycopy_symbol, *byref_symbol, *const_symbol, *void_symbol, *star_symbol, *id_symbol, *voidstar_symbol, *idstar_symbol, *int_symbol, *BOOL_symbol, *double_symbol, *float_symbol, *NSRect_symbol, *NSPoint_symbol, *NSSize_symbol, *NSRange_symbol, *SEL_symbol, *Class_symbol;
 
 static void prepare_symbols(NuSymbolTable *symbolTable)
@@ -1761,3 +1781,184 @@ id help_add_method_to_class(Class classToExtend, id cdr, NSMutableDictionary *co
         return nil;
     }
 }
+
+#ifdef __BLOCKS__
+
+static id make_cblock (NuBlock *nuBlock, NSString *signature);
+static void objc_calling_nu_block_handler(ffi_cif* cif, void* returnvalue, void** args, void* userdata);
+char **generate_block_userdata(NuBlock *nuBlock, const char *signature);
+void *construct_block_handler(NuBlock *block, const char *signature);
+
+@implementation NuBridgedBlock
+
++(id)cBlockWithNuBlock:(NuBlock*)nb signature:(NSString*)sig
+{
+	return [[[[self alloc] initWithNuBlock:nb signature:sig] autorelease] cBlock];
+}
+
+-(id)initWithNuBlock:(NuBlock*)nb signature:(NSString*)sig
+{
+	nuBlock = [nb retain];
+	cBlock = make_cblock(nb,sig);
+	
+	return self;
+}
+
+-(NuBlock*)nuBlock
+	{return [[nuBlock retain] autorelease];}
+
+-(id)cBlock
+	{return [[cBlock retain] autorelease];}
+
+-(void)dealloc
+{
+	[nuBlock release];
+	[cBlock release];
+	[super dealloc];
+}
+
+@end
+typedef int(*ftype)(size_t,int,int);
+//the caller gets ownership of the block
+static id make_cblock (NuBlock *nuBlock, NSString *signature)
+{
+	void *funcptr = construct_block_handler(nuBlock, [signature UTF8String]);
+	
+
+	void(^cBlock)(void)=^(void){printf("dookie");};
+	#ifdef __x86_64__
+	*((size_t*)(id)cBlock + 2) = (size_t)funcptr;
+	#else
+	*((size_t*)(id)cBlock + 3) = (size_t)funcptr;
+	#endif
+	return cBlock;
+}
+
+static void objc_calling_nu_block_handler(ffi_cif* cif, void* returnvalue, void** args, void* userdata)
+{
+	//Debugger();
+    int argc = cif->nargs - 1;
+	//void *ptr = (void*)args[0]  //don't need this first parameter
+    // see objc_calling_nu_method_handler
+	
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    
+    NuBlock *block = ((NuBlock **)userdata)[1];
+    //NSLog(@"----------------------------------------");
+    //NSLog(@"calling block %@", [block stringValue]);
+    id arguments = [[NuCell alloc] init];
+    id cursor = arguments;
+    int i;
+    for (i = 0; i < argc; i++) {
+        NuCell *nextCell = [[NuCell alloc] init];
+        [cursor setCdr:nextCell];
+        [nextCell release];
+        cursor = [cursor cdr];
+        id value = get_nu_value_from_objc_value(args[i+1], ((char **)userdata)[i+2]);
+        [cursor setCar:value];
+    }
+	//NSLog(@"in nu method handler, using arguments %@", [arguments stringValue]);
+    id result = [block evalWithArguments:[arguments cdr] context:nil];
+    //NSLog(@"in nu method handler, putting result %@ in %x with type %s", [result stringValue], (size_t) returnvalue, ((char **)userdata)[0]);
+    char *resultType = (((char **)userdata)[0])+1;// skip the first character, it's a flag
+    set_objc_value_from_nu_value(returnvalue, result, resultType);
+    [arguments release];
+    if (pool) {
+        if (resultType[0] == '@')
+            [*((id *)returnvalue) retain];
+        [pool release];
+        if (resultType[0] == '@')
+            [*((id *)returnvalue) autorelease];
+    }
+}
+
+
+char **generate_block_userdata(NuBlock *nuBlock, const char *signature)
+{
+    NSMethodSignature *methodSignature = [NSMethodSignature signatureWithObjCTypes:signature];
+    const char *return_type_string = [methodSignature methodReturnType];
+    int argument_count = [methodSignature numberOfArguments];
+    char **userdata = (char **) malloc ((argument_count+3) * sizeof(char*));
+    userdata[0] = (char *) malloc (2 + strlen(return_type_string));
+
+	//assume blocks never return retained results
+	sprintf(userdata[0], " %s", return_type_string);
+    
+	//so first element is return type, second is nuBlock
+    userdata[1] = (char *) nuBlock;
+    [nuBlock retain];
+    int i;
+    for (i = 0; i < argument_count; i++) {
+        const char *argument_type_string = [methodSignature getArgumentTypeAtIndex:i];
+        userdata[i+2] = strdup(argument_type_string);
+    }
+    userdata[argument_count+2] = NULL;
+	
+#if 0
+	NSLog(@"Userdata for block: %@, signature: %s", [nuBlock stringValue], signature);
+	for (int i = 0; i < argument_count+2; i++)
+	{	if (i != 1)
+			NSLog(@"userdata[%i] = %s",i,userdata[i]);	}
+#endif
+    return userdata;
+}
+
+
+void *construct_block_handler(NuBlock *block, const char *signature)
+{
+    char **userdata = generate_block_userdata(block, signature);
+    
+	//implement a handlerWarehouse in the future
+	//IMP imp = [NuHandlerWarehouse handlerWithSelector:sel block:block signature:signature userdata:userdata];
+  //  if (imp) {
+   //     return imp;
+    //}
+    int argument_count = 0;
+    while (userdata[argument_count] != 0) argument_count++;
+	argument_count-=1; //unlike a method call, c blocks have one, not two hidden args
+#if 0
+	NSLog(@"using libffi to construct handler for nu block with %d arguments and signature %s", argument_count, signature);
+#endif
+	
+	
+	
+    ffi_type **argument_types = (ffi_type **) malloc ((argument_count+1) * sizeof(ffi_type *));
+    ffi_type *result_type = ffi_type_for_objc_type(userdata[0]+1);
+
+    argument_types[0] = ffi_type_for_objc_type("^?");
+	
+    for (int i = 1; i < argument_count; i++)
+        argument_types[i] = ffi_type_for_objc_type(userdata[i+1]);
+    argument_types[argument_count] = NULL;
+    ffi_cif *cif = (ffi_cif *)malloc(sizeof(ffi_cif));
+    if (cif == NULL) {
+        NSLog(@"unable to prepare closure for signature %s (could not allocate memory for cif structure)", signature);
+        return NULL;
+    }
+    int status = ffi_prep_cif(cif, FFI_DEFAULT_ABI, argument_count, result_type, argument_types);
+    if (status != FFI_OK) {
+        NSLog(@"unable to prepare closure for signature %s (ffi_prep_cif failed)", signature);
+        return NULL;
+    }
+    ffi_closure *closure = (ffi_closure *)mmap(NULL, sizeof(ffi_closure), PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+    if (closure == (ffi_closure *) -1) {
+        NSLog(@"unable to prepare closure for signature %s (mmap failed with error %d)", signature, errno);
+        return NULL;
+    }
+    if (closure == NULL) {
+        NSLog(@"unable to prepare closure for signature %s (could not allocate memory for closure)", signature);
+        return NULL;
+    }
+    if (ffi_prep_closure(closure, cif, objc_calling_nu_block_handler, userdata) != FFI_OK) {
+        NSLog(@"unable to prepare closure for signature %s (ffi_prep_closure failed)", signature);
+        return NULL;
+    }
+    if (mprotect(closure, sizeof(closure), PROT_READ | PROT_EXEC) == -1) {
+        NSLog(@"unable to prepare closure for signature %s (mprotect failed with error %d)", signature, errno);
+        return NULL;
+    }
+    return (void*)closure;
+}
+
+#endif //__BLOCKS__
+
