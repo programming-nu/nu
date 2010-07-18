7fe944d9d53492f9ce94645785b2a46f9bc042c9
Tim Burks <tim@neontology.com>
2010-07-17 22:00:59 -0700

    bumping version number.

--- objc/version.h
+++ objc/version.h
@@ -1,8 +1,8 @@
-#define NU_VERSION "0.9.0"
+#define NU_VERSION "0.9.1"
 #define NU_VERSION_MAJOR 0
 #define NU_VERSION_MINOR 9
-#define NU_VERSION_TWEAK 0
-#define NU_RELEASE_DATE "2010-7-2"
+#define NU_VERSION_TWEAK 1
+#define NU_RELEASE_DATE "2010-7-17"
 #define NU_RELEASE_YEAR  2010
 #define NU_RELEASE_MONTH 7
-#define NU_RELEASE_DAY   2
+#define NU_RELEASE_DAY   17
