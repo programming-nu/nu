be0d3671a7fd06751c567cb4d73c782f474e84a9
Tim Burks <timburks@google.com>
2017-04-13 07:59:48 -0700

    Set Nu__null = [NSNull null]

--- objc/Nu.m
+++ objc/Nu.m
@@ -249,7 +249,7 @@ void NuInit()
     initialized = YES;
     @autoreleasepool {
         // as a convenience, we set a file static variable to nil.
-        Nu__null = Nu__null;
+        Nu__null = [NSNull null];
         
         // add enumeration to collection classes
         [NSArray include: [NuClass classWithClass:[NuEnumerable class]]];
